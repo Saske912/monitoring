431ce8a9e6d8c548c74bff1661d2c5504c5a4706
Saske912 <saveloy@yandex.ru>
2023-07-08 23:28:08 +0300

    user management

--- grafana-configure/OSS.tf
+++ grafana-configure/OSS.tf
@@ -6,22 +6,87 @@ locals {
 }
 
 
-resource "grafana_data_source" "inflxudb" {
-  type        = "influxdb"
-  name        = "influxDBv2"
-  url         = "http://influxdb.metrics:8086"
-  access_mode = "proxy"
-  json_data_encoded = jsonencode({
-    version       = "Flux"
-    orgranization = local.influxdb["organization"]
-    defaultBucket = local.influxdb["bucket"]
-    token         = local.influxdb["token"]
-  })
+# resource "grafana_data_source" "inflxudb" {
+#   type        = "influxdb"
+#   name        = "influxDBv2"
+#   url         = "http://influxdb.metrics:8086"
+#   access_mode = "proxy"
+#   json_data_encoded = jsonencode({
+#     version       = "Flux"
+#     orgranization = local.influxdb["organization"]
+#     defaultBucket = local.influxdb["bucket"]
+#     token         = local.influxdb["token"]
+#   })
+# }
+
+# resource "grafana_data_source" "prometheus" {
+#   type       = "prometheus"
+#   name       = "Prometheus"
+#   url        = "http://prometheus-server.prometheus"
+#   is_default = true
+# }
+
+data "vault_generic_secret" "users" {
+  path = "kv/kolve/org/personal"
 }
 
-resource "grafana_data_source" "prometheus" {
-  type       = "prometheus"
-  name       = "Prometheus"
-  url        = "http://prometheus-server.prometheus"
-  is_default = true
+data "vault_generic_secret" "teams" {
+  path = "kv/kolve/org/teams"
+}
+
+data "vault_generic_secret" "info" {
+  path = "kv/kolve/org/info"
+}
+
+locals {
+  mihail        = jsondecode(data.vault_generic_secret.users.data["mihail"])
+  my-cat        = jsondecode(data.vault_generic_secret.users.data["my-cat"])
+  spaceShifters = jsondecode(data.vault_generic_secret.teams.data["spaceShifters"])
+}
+
+resource "grafana_user" "user" {
+
+  email    = local.mihail.email
+  name     = local.mihail.name
+  login    = local.mihail.login
+  password = local.mihail.password
+  is_admin = local.mihail.admin
+}
+
+resource "grafana_user" "cat" {
+
+  email    = local.my-cat.email
+  name     = local.my-cat.name
+  login    = local.my-cat.login
+  password = local.my-cat.password
+  is_admin = local.my-cat.admin
+}
+
+resource "grafana_team" "team" {
+
+  name    = "spaceShifters"
+  members = local.spaceShifters
+  preferences {
+    timezone = "browser"
+  }
+}
+
+resource "grafana_folder" "folder" {
+  title = data.vault_generic_secret.info.data["name"]
+}
+
+resource "grafana_folder_permission" "collectionPermission" {
+  folder_uid = grafana_folder.folder.uid
+  permissions {
+    role       = "Editor"
+    permission = "Edit"
+  }
+  permissions {
+    team_id    = grafana_team.team.id
+    permission = "View"
+  }
+  permissions {
+    user_id    = grafana_user.user.id
+    permission = "Admin"
+  }
 }
--- main.tf
+++ main.tf
@@ -67,6 +67,10 @@ data "vault_generic_secret" "mail" {
   path = "kv/mail"
 }
 
+data "vault_generic_secret" "influx" {
+  path = "kv/influxdb"
+}
+
 locals {
   grafana = data.vault_generic_secret.graf.data
   mail    = jsondecode(data.vault_generic_secret.graf.data["mail"])
@@ -117,73 +121,6 @@ resource "helm_release" "grafana" {
   }
   values = [templatefile("grafanaValues.yml", { domain = local.grafana["domain"],
     mail         = data.vault_generic_secret.mail.data, grafana = local.grafana,
-    grafana-mail = local.mail
+    grafana-mail = local.mail, influx = data.vault_generic_secret.influx.data
   })]
 }
-
-
-
-# data "vault_generic_secret" "minio" {
-#   path = "kv/minio"
-# }
-
-# data "vault_generic_secret" "cluster" {
-#   path = "kv/cluster"
-# }
-
-# resource "helm_release" "loki" {
-#   chart            = "loki"
-#   name             = "loki"
-#   namespace        = "loki"
-#   repository       = "https://grafana.github.io/helm-charts"
-#   create_namespace = true
-#   version          = "5.8.9"
-#   set {
-#     name  = "loki.storage.s3.endpoint"
-#     value = data.vault_generic_secret.minio.data["host"]
-#   }
-#   set {
-#     name  = "loki.storage.s3.region"
-#     value = data.vault_generic_secret.minio.data["region"]
-#   }
-#   set {
-#     name  = "loki.storage.s3.secretAccessKey"
-#     value = data.vault_generic_secret.minio.data["secret_key"]
-#   }
-#   set {
-#     name  = "loki.storage.s3.accessKeyId"
-#     value = data.vault_generic_secret.minio.data["key"]
-#   }
-#   set {
-#     name  = "test.prometheusAddress"
-#     value = "http://prometheus-server.prometheus"
-#   }
-#   set {
-#     name  = "global.clusterDomain"
-#     value = data.vault_generic_secret.cluster.data["domain"]
-#   }
-#   set {
-#     name  = "write.replicas"
-#     value = 1
-#   }
-#   set {
-#     name  = "write.autoscaling.enabled"
-#     value = true
-#   }
-#   set {
-#     name  = "read.replicas"
-#     value = 1
-#   }
-#   set {
-#     name  = "read.autoscaling.enabled"
-#     value = true
-#   }
-#   set {
-#     name  = "backend.replicas"
-#     value = 1
-#   }
-#   set {
-#     name  = "backend.autoscaling.enabled"
-#     value = true
-#   }
-# }
